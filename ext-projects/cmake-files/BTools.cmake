140a766da8db9e263bd75db316b94db057da1664
danoan <danoan2008@gmail.com>
2019-09-30 09:32:07 +0200

    CMake configuration fixes.

--- ext-projects/cmake-files/BTools.cmake
+++ ext-projects/cmake-files/BTools.cmake
@@ -1,6 +1,6 @@
 ExternalProject_Add(btools
         GIT_REPOSITORY https://github.com/danoan/BTools.git
-        GIT_TAG v0.1
+        GIT_TAG master
         SOURCE_DIR ${EXTPROJECTS_SOURCE_DIR}/BTools
         CMAKE_ARGS
         -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}
