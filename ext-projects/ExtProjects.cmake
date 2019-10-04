54da1f8816ac449aff69d2e4a3717c52e9955c58
danoan <danoan2008@gmail.com>
2019-10-04 18:08:44 +0200

    SCaBOliC alternative version for maxEnergy-study

--- ext-projects/ExtProjects.cmake
+++ ext-projects/ExtProjects.cmake
@@ -2,6 +2,8 @@ include(ExternalProject)
 
 include(ext-projects/cmake-files/DIPaCUS.cmake)
 include(ext-projects/cmake-files/SCaBOliC.cmake)
+#If building maxEnergy-study, use grid-adjustement version of SCaBOliC
+#include(ext-projects/cmake-files/SCaBOliC-grid-adjustement.cmake)
 include(ext-projects/cmake-files/GEOC.cmake)
 include(ext-projects/cmake-files/GCurve.cmake)
 include(ext-projects/cmake-files/magLac.cmake)
